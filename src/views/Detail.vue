dd0c7397d71c3334e90061ee2129fde61b6db624
e <18702707106@163.com>
2020-05-18 16:00:57 +0800

    代码优化

--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -1,33 +1,29 @@
 <template>
   <div class="qui-page qui-fx-ver">
     <div class="header qui-fx-ac qui-fx-jc">
-      <span @click="goBack">返回</span>
-      <span class="qui-fx-f1 qui-fx-jc">详情</span>
+      <span>返回</span>
+      <span class="qui-fx-f1">详情</span>
     </div>
-    <div class="qui-fx-f1"></div>
+    <div class="qui-fx-f1">
+      <div>名称：{{}}</div>
+    </div>
   </div>
 </template>
 
 <script>
-import { reactive, getCurrentInstance } from 'vue'
+import { reactive } from 'vue'
 export default {
   name: 'Detail',
   components: {},
   setup() {
-    const { ctx } = getCurrentInstance()
-    console.log(ctx.$router.currentRoute.value.query)
     const detail = reactive({
       id: 1,
       title: '',
       des: '',
       time: ''
     })
-    const goBack = () => {
-      ctx.$router.back()
-    }
     return {
-      detail,
-      goBack
+      detail
     }
   }
 }
